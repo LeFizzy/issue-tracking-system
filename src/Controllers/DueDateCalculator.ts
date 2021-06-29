0a771056dafda12b07eaeb45d5c2e2073178a263
LeFizzy <contact@milanzeisler.hu>
2021-06-29 14:37:34 +0200

    Add string to date conversion method.

--- src/Controllers/DueDateCalculator.ts
+++ src/Controllers/DueDateCalculator.ts
@@ -1,6 +1,17 @@
 import App from "../AppClass";
 
 /**
+    * Convert StringTime To Date Object.
+    * @param {String} inputDateString
+    * @returns {String} Date
+*/
+const ConvertStringTimeToDateObject = (inputDateString: string) => {
+    let date = new Date(inputDateString);
+
+    return date;
+}
+
+/**
     * Calculate Due Date method.
     * @param {String} submitDateTime
     * @param {String} turnaroundTime
