4b7a6bc71aca254fbee478b68e2f01cd2f14477d
LeFizzy <contact@milanzeisler.hu>
2021-06-29 14:18:59 +0200

    Add method comments & parameters.

--- src/Controllers/DueDateCalculator.ts
+++ src/Controllers/DueDateCalculator.ts
@@ -1,5 +1,11 @@
 import App from "../AppClass";
 
-export function CalculateDueDate(this: App) {
+/**
+    * Calculate Due Date method.
+    * @param {String} submitDateTime
+    * @param {String} turnaroundTime
+    * @returns {String} dueDate
+*/
+export function CalculateDueDate(this: App, submitDateTime: string, turnaroundTime: string) {
     return true;
 }
